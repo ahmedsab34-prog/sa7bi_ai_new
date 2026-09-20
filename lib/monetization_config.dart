394b5c34709d66b0b0eb679ab336df46a0b3aeab
ahmedsab34-prog <ahmedsab34@gmail.com>
2026-09-20 17:39:28 +0300

    Create monetization_config.dart

---
+++ lib/monetization_config.dart
@@ -0,0 +1,29 @@
+class MonetizationConfig {
+  MonetizationConfig._();
+
+  static const String appDownloadUrl =
+      'https://sa7bi-ai-new.ahmedsab34.workers.dev/download';
+
+  static const String amazonUrl =
+      'https://www.amazon.eg/';
+
+  static const String jumiaUrl =
+      'https://www.jumia.com.eg/';
+
+  static const String noonUrl =
+      'https://www.noon.com/egypt-ar/';
+
+  static const String facebookShopUrl =
+      'https://www.facebook.com/marketplace/';
+
+  /*
+   * دي روابط المتاجر الحالية وليست Affiliate Links.
+   *
+   * بعد إنشاء حسابات Affiliate الحقيقية،
+   * نستبدل القيم فقط بدون تغيير تصميم التطبيق.
+   */
+
+  static const bool affiliateEnabled = true;
+
+  static const bool admobEnabled = false;
+}
